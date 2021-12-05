ceb137d9bafa2024319dcccdc672e6a94b807153
Eralp Karaduman <eralp@eralpkaraduman.com>
2021-12-06 01:42:09 +0200

    Connect metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Web3 from "web3";
+
+interface Web3ControllerEventListener {
+  onUpdate: () => void;
+}
+
+class Web3Controller {
+  private web3 = new Web3(Web3.givenProvider);
+
+  private eventListeners: Web3ControllerEventListener[] = [];
+
+  private _accounts: string[] = [];
+  public get accounts(): string[] {
+    return this._accounts;
+  }
+
+  public async requestAccounts() {
+    this._accounts = await this.web3.eth.requestAccounts();
+    this.dispatchEvent("onUpdate");
+  }
+
+  public addEventListener(listener: Web3ControllerEventListener) {
+    this.eventListeners.push(listener);
+  }
+
+  private dispatchEvent(event: keyof Web3ControllerEventListener) {
+    this.eventListeners.forEach((listener) => listener[event]());
+  }
+}
 
 function App() {
+  const web3Ref = React.useRef<Web3Controller>();
+  const [accounts, setAccounts] = React.useState<string[]>([]);
+  const [selectedAccount, setSelectedASccount] = React.useState<
+    string | undefined
+  >();
+  React.useEffect(() => {
+    web3Ref.current = new Web3Controller();
+    web3Ref.current.addEventListener({
+      onUpdate() {
+        setAccounts(web3Ref.current?.accounts ?? []);
+        setSelectedASccount(web3Ref.current?.accounts[0]);
+      },
+    });
+  }, []);
+  const handleOnClick = () => {
+    web3Ref.current?.requestAccounts();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleOnClick}>Connect Metamask</button>
+      {selectedAccount && <h1>{selectedAccount}</h1>}
     </div>
   );
 }
